813b99ac12dd83a56a917b82305a779ad0a98bed
Jeremy Davis <jeremy.davis2@hcahealthcare.com>
2020-11-24 15:04:34 -0600

    lexicon and brand architecture responsive updates and CSS cleanup

--- script/sidebar.js
+++ script/sidebar.js
@@ -1,15 +1,4 @@
 $(document).ready(function(e) {
-    // Toggle Open Class on Sidebar Buttons
-
-    $(".sidebar-left button").click( function(e) {
-        if(!$(this).hasClass('open')) {
-            $(".sidebar-left button").removeClass("open");
-            $(this).addClass("open");
-        } else {
-            $(".sidebar-left button").removeClass("open");
-        }
-    });
-    
     // Make Sidebar Nav sticky when top of window reaches it 
 
     var sidebarPosition = $('.sidebar-left').position();
